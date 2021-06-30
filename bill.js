571af6da9e3ef5e81d0da94ce41a751db722afa8
emarcins <emarcins@gmail.com>
2021-06-30 23:28:05 +0200

    refactoring - mobile version

--- bill.js
+++ bill.js
@@ -1,29 +1,53 @@
-const price = document.querySelector('#price');
-const people = document.querySelector('#people');
-const tip = document.querySelector('#tip');
-const button = document.querySelector('.count');
-const error = document.querySelector('.error');
-const info = document.querySelector('.cost-info');
-const cost = document.querySelector('span');
+const price = document.querySelector("#price");
+const people = document.querySelector("#people");
+const tip = document.querySelector("#tip");
+const currency = document.querySelector("#currency");
+const countBtn = document.querySelector(".count");
+const clearBtn = document.querySelector(".clear");
+const error = document.querySelector(".error");
+const info = document.querySelector(".cost-info");
+const cost = document.querySelector(".cost");
+const tipInfo = document.querySelector(".tip-info");
+const tipValue = document.querySelector(".tip-value");
 
 const bill = () => {
-    const newPrice = parseFloat(price.value);
-    const newPeople = parseInt(people.value); 
-    const newTip = parseFloat(tip.value);
-    const sum = (newPrice + (newPrice * newTip))/ newPeople;
-    info.style.display = 'block';
-    cost.textContent = sum.toFixed(2);
+  const newPrice = parseFloat(price.value);
+  const newPeople = parseInt(people.value);
+  const newTip = parseFloat(tip.value);
+  const sum = (newPrice + newPrice * newTip) / newPeople;
+  const tipSum = newPrice * newTip;
+  const currencyType = currency.value;
+  info.style.display = "block";
+  cost.textContent = `${sum.toFixed(2)} ${currencyType}`;
+  tipInfo.style.display = "block";
+  tipValue.textContent = `${tipSum.toFixed(2)} ${currencyType}`;
 };
 
 const checkPrice = () => {
-    if(price.value == '' || people.value == '' || tip.value == 0){
-        error.textContent = 'Wprowadź odpowiednią wartość!' 
-        info.style.display = 'none'
-    } else {  
-        error.textContent = ''      
-        bill()
-    }
+  if (
+    price.value == "" ||
+    people.value == "" ||
+    tip.value == 0 ||
+    currency.value == 0
+  ) {
+    error.textContent = "Please enter the appropriate value!";
+    info.style.display = "none";
+    tipInfo.style.display = "none";
+  } else {
+    error.textContent = "";
+    bill();
+  }
 };
 
-button.addEventListener('click', checkPrice)
+const clearValues = () => {
+  price.value = "";
+  people.value = "";
+  tip.value = 0;
+  currency.value = 0;
+  info.style.display = "none";
+  tipInfo.style.display = "none";
+  error.textContent = "";
+};
 
+countBtn.addEventListener("click", checkPrice);
+clearBtn.addEventListener("click", clearValues);
